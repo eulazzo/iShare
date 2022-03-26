55b0e37c31678ffa135b2beb89625f4c960811ac
Lázaro Vanderson <eulazzo@protonmail.com>
2022-03-25 22:34:57 -0300

    setup router D

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
           </div>
         )}
       </div>
-      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+      <div className="pb-2 flex-2 h-screen overflow-y-scroll" ref={scrollRef}>
         <Routes>
           <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/*" element={<Pins user={user && user} />} />
